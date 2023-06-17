8951132fa4e4f1ba47ef644ab69411c3c0167544
Ash-2k3 <ash.me.cse@gmail.com>
2023-06-17 20:45:39 +0530

    Demo pr

--- app/controller/controllers.py
+++ app/controller/controllers.py
@@ -5,8 +5,14 @@ import requests
 
 
 get_index_bp = Blueprint('index', __name__)
+get_sign_in = Blueprint('sign_in',__name__)
 
 @get_index_bp.route('/')
 def index():
            # Render index.html
            return render_template('index.html')
+
+@get_sign_in.route('/sign-in')
+def sign_in():
+           # Render sign-in.html
+           return render_template('sign-in.html')
