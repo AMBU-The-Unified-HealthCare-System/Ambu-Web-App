db7ee2cf0864eb180eae3cdc70110c77bd5624d3
onlyVishesh <Visheshmaurya14@gmail.com>
2023-06-26 18:31:22 +0530

    Solve marge conflicts

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,9 @@ from app.controller import controllers
 def create_app():
     app = Flask(__name__)
     app.config['DEBUG'] = True
-    
+
     # Register the blueprints for endpoints in app instance
     app.register_blueprint(controllers.get_index_bp)
     app.register_blueprint(controllers.get_Sign_Up)
+    app.register_blueprint(controllers.get_about_page)
     return app
-
-
-
--- app/controller/controllers.py
+++ app/controller/controllers.py
@@ -5,13 +5,23 @@ import requests
 
 
 get_index_bp = Blueprint('index', __name__)
-get_Sign_Up = Blueprint('Sign_Up',__name__)
+get_Sign_Up = Blueprint('Sign_Up', __name__)
+get_about_page = Blueprint('about-page', __name__)
+
 
 @get_index_bp.route('/')
 def index():
-           # Render index.html
-           return render_template('index.html')
+    # Render index.html
+    return render_template('index.html')
+
+
 @get_Sign_Up.route('/Sign_Up')
 def index():
-          # Render Sign_Up.html
-          return render_template('Sign_Up.html')
+    # Render Sign_Up.html
+    return render_template('Sign_Up.html')
+
+
+@get_about_page.route('/about-page')
+def index():
+    # Render about-page.html
+    return render_template('about-page.html')
