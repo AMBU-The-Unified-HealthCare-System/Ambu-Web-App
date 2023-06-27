fe72ba4a8e2b081bc0e74b5fa234709f89b46aca
Vishesh <121187728+onlyVishesh@users.noreply.github.com>
2023-06-27 18:04:20 +0530

    Added all my about page Task source code in flask app and it is working (#15)
    * Added all my about page files in flask app and it is working
    
    i have added all my about page source code in flask app which is working from my end i also added video of both disktop and mobile view of my website
    
    * Made all changes as told
    
    * Added old default code
    
    * Solve marge conflicts

--- app/controller/controllers.py
+++ app/controller/controllers.py
@@ -3,15 +3,24 @@ from flask import Blueprint, render_template
 import json
 import requests
 
-
 get_index_bp = Blueprint('index', __name__)
-get_Sign_Up = Blueprint('Sign_Up',__name__)
+get_Sign_Up = Blueprint('Sign_Up', __name__)
+get_about_page_bp = Blueprint('about_page', __name__)
+
 
 @get_index_bp.route('/')
 def index():
-           # Render index.html
-           return render_template('index.html')
+    # Render index.html
+    return render_template('index.html')
+
+
 @get_Sign_Up.route('/Sign_Up')
 def index():
-          # Render Sign_Up.html
-          return render_template('Sign_Up.html')
+    # Render Sign_Up.html
+    return render_template('Sign_Up.html')
+
+
+@get_about_page_bp.route('/about')
+def index():
+    # Render Sign_Up.html
+    return render_template('about-page.html')
